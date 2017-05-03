90f0931c2b0a8e8a078952ac3adfe8c116a9f3ef
chutikarn <chutikarn@berkeley.edu>
2017-05-03 12:22:29 -0700

    Added popup

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-$function().
+$(document).ready(function(){
+
+    $('#activator').click(function(){
+        $('#overlay').fadeIn(200,function(){
+            $('.box').addClass('showbox');
+        });
+        return false;
+    });
+    $('#exit').click(function(){
+        $('.box').removeClass('showbox');
+        $('#overlay').fadeOut('fast');
+    });
+    
+ 	$('#activator').hover(
+ 	function(){
+ 		$('#activator').css("color", "black");
+ 	},
+ 	function(){
+ 		$('#activator').css("color", "white");
+ 	});
+
+});
